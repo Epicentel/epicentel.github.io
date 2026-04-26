344f5b8e618b1afe63e1c9eb5f73d73111f84215
epicentel <epifa@pipis>
2026-04-26 14:35:57 -0700

    deploy angular cv app

---
+++ src/app/models/education/education.model.ts
@@ -0,0 +1,7 @@
+  export class Education {
+    id?: string;
+    startDate?: string = 'mmm-yyyy';
+    endDate: string = 'mmm-yyyy';
+    institution?: string = 'institution';
+    degree?: string = 'degree';
+  }
---
+++ src/app/models/skills/skills.modelo.ts
@@ -0,0 +1,7 @@
+1  export class Skills {
+2    id?: string;
+3    Disciplina?: string = 'Disciplina';
+4    Herramientas IA?: string = 'Herramientas IA';
+5    Inteligencia social?: string = 'Inteligencia social';
+6    Manejo de office?: string = 'Manejo de office';
+7 }
